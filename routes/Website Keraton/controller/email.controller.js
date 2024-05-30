8e8ea61e5626cde1f5077eecc7aa9244c66f1125
Harmoni Natanael S <harmoninatanaels@gmail.com>
2024-05-30 14:35:35 +0700

    dbjkdasjkas

--- routes/Website Keraton/controller/email.controller.js
+++ routes/Website Keraton/controller/email.controller.js
@@ -2,16 +2,29 @@ var express = require("express");
 var router = express.Router();
 const path = require('path')
 const Email = require('../emails/email');
+require('dotenv').config()
 const { error } = require("../../utils/response");
-const emailClass = new Email()
+const transactionModel = require('../models/transaction.models');
+const { prisma } = require("../../utils/prisma");
 
 router.get('/render', async (req, res) => {
-    try{
+    try {
         res.render(path.resolve('routes/Website Keraton/emails/templates/invoice'), {
             email: "Email Here",
             name: "Name Here"
         })
-    }catch(err){
+    } catch (err) {
+        return error(res, err.message)
+    }
+})
+
+router.post('/invoice/:id', async (req, res) => {
+    try {
+        const transactionExist = await prisma.transaction.findFirstOrThrow({ where: { id: req.params.id } })
+        if (!transactionExist) throw Error('Transaction Didnt Exist')
+        const emailClass = new Email(process.env.EMAIL_FROM, )
+        emailClass.sendEmailTemplate()
+    } catch (err) {
         return error(res, err.message)
     }
 })
