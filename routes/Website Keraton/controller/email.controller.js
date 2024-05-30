bb6a41e2d14c6788d01065f054935c904c14124b
Nadif Dzaikra <nadifdzaikra@gmail.com>
2024-05-30 15:02:42 +0700

    kung

--- routes/Website Keraton/controller/email.controller.js
+++ routes/Website Keraton/controller/email.controller.js
@@ -2,8 +2,9 @@ var express = require("express");
 var router = express.Router();
 const path = require('path')
 const Email = require('../emails/email');
-const { error } = require("../../utils/response");
+const { error, success } = require("../../utils/response");
 const { userInfo } = require("os");
+const { splitDate } = require("../../utils/helper");
 const emailClass = new Email()
 
 router.get('/render', async (req, res) => {
@@ -39,35 +40,28 @@ router.get('/render', async (req, res) => {
 
 router.post('/invoice/:id', async (req, res) => {
     try {
-        const transactionExist = await prisma.transaction.findFirstOrThrow({ where: { id: req.params.id }, include: { user: true } })
+        const transactionExist = await prisma.transaction.findFirstOrThrow({ where: { id: req.params.id }, include: { user: true, detailTrans: { include: { order: true, event: true } } } })
         if (!transactionExist) throw Error('Transaction Didnt Exist')
         const emailClass = new Email(process.env.EMAIL_FROM, "nadifdzaikra@gmail.com", "Test")
-        emailClass.sendEmailTemplate(
+        await emailClass.sendEmailTemplate(
             {
-                email: transactionExist.,
-                name: "Steve",
-                date: new Date().toLocaleDateString(),
-                nomor_invoice: 109109019020,
-                method: method,
-                invoices: [
-                    {
-                        item_desc: "test",
-                        quantity: "test",
-                        price: "test",
-                        total: "test",
-                    },
-                    {
-                        item_desc: "test",
-                        quantity: "test",
-                        price: "test",
-                        total: "test",
-                    }
-                ],
-                subtotal: 2000,
-                tax: 1000,
-                total: 3000
+                email: transactionExist.user.email,
+                name: transactionExist.user.name,
+                date: splitDate(transactionExist.plannedDate)[0],
+                nomor_invoice: transactionExist.id,
+                method: transactionExist.method,
+                invoices: transactionExist.detailTrans.map(detail => ({
+                    item_desc: detail.order.desc,
+                    quantity: detail.amount,
+                    price: detail.orderId ? detail.order.price : detail.event.price,
+                    total: detail.amount * detail.orderId ? detail.order.price : detail.event.price,
+                })),
+                subtotal: transactionExist.total,
+                tax: transactionExist.additionalFee,
+                total: transactionExist.total + transactionExist.additionalFee
             }
         )
+        return success(res, "Email terkirim", 201)
     } catch (err) {
         return error(res, err.message)
     }
