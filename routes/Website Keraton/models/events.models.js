97b1bc63fbf115f70f729446b7cf50fd85cf1015
Harmoni Natanael S <harmoninatanaels@gmail.com>
2024-05-30 12:50:34 +0700

    dasdasd

--- routes/Website Keraton/models/events.models.js
+++ routes/Website Keraton/models/events.models.js
@@ -4,9 +4,9 @@ const eventIterationModel = require('../models/eventIteration.models')
 
 
 const isExist = async (id) => {
-    try{
-        return await prisma.events.findFirst({ where: { id }})
-    }catch(err){
+    try {
+        return await prisma.events.findFirst({ where: { id } })
+    } catch (err) {
         throwError(err)
     }
 }
@@ -14,11 +14,11 @@ const isExist = async (id) => {
 const getAll = async (query) => {
     let { iterat, free } = query
     try {
-        console.log(query)
+        console.log(free)
         return await prisma.events.findMany({
             where: {
                 ...(iterat && { iterationId: { in: iterat } }),
-                ...(free && { isFree: free })
+                ...(free != undefined && { isFree: free })
             },
             include: { iteration: true }
         })
@@ -28,43 +28,51 @@ const getAll = async (query) => {
 }
 
 const getOne = async (id) => {
-    try{
+    try {
         return await prisma.events.findFirstOrThrow({
             where: { id }
         })
-    }catch(err){
+    } catch (err) {
         throwError(err)
     }
 }
 
 const iterationIdExist = async (id) => {
-    try{
+    try {
         const isExist = await eventIterationModel.isExist(+id)
-        if(!isExist) throw Error('Iteration Id didnt exist')
-    }catch(err){
+        if (!isExist) throw Error('Iteration Id didnt exist')
+    } catch (err) {
         throwError(err)
     }
 }
 
 const create = async (data) => {
-    try{
+    try {
         await iterationIdExist(+data.iterationId)
         return await prisma.events.create({ data })
-    }catch(err){
+    } catch (err) {
         throwError(err)
     }
 }
 
 const update = async (id, data) => {
-    try{
+    try {
         if (data.iterationId) await iterationIdExist(data.iterationId)
         const exist = await isExist(id)
-        if(!exist) throw Error('Event Id didnt exist')
+        if (!exist) throw Error('Event Id didnt exist')
         return await prisma.events.update({ where: { id }, data })
+    } catch (err) {
+        throwError(err)
+    }
+}
+
+const deleteData = async (id) => {
+    try{
+        return await prisma.events.delete({ where: { id } })
     }catch(err){
         throwError(err)
     }
 }
 
 
-module.exports = { isExist, getOne, getAll, create, update }
+module.exports = { isExist, getOne, getAll, create, update, deleteData }
