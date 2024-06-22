9e4e19d0a997c218dd3a093b4d9643c74f7db871
Harmoni Natanael S <harmoninatanaels@gmail.com>
2024-06-22 23:52:47 +0700

    adsjsdjksd

--- routes/Keraton PoS/controller/keratonTransaction.controller.js
+++ routes/Keraton PoS/controller/keratonTransaction.controller.js
@@ -2,11 +2,15 @@ var express = require("express");
 const { error, success } = require("../../utils/response");
 var router = express.Router();
 const keratonTransactionModel = require('../models/keratonTransaction.models')
-
+const transactionModel = require('../models/transaction.models')
 
 // curaweda-income/transfer (Buat button)
 router.post('/transfer', async (req, res) => {
+    let curawedaTransferAmount = 0
     try{
+        const curawedaIncome = await transactionModel.getRevenueCurawedaKeraton()
+        curawedaTransferAmount += curawedaIncome.revenueCuraweda.COH + curawedaIncome.revenueCuraweda.CIA
+        req.body.paymentAmount = curawedaTransferAmount
         const data = await keratonTransactionModel.createTransafer(req.body)
         return success(res, 'Transfer Successfully', data)
     }catch(err){
