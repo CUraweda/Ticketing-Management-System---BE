dca83c5c4d257bd5650209ffae53dbe554030a5f
Harmoni Natanael S <harmoninatanaels@gmail.com>
2024-09-26 18:22:57 +0700

    Fix validate

--- routes/Website Keraton/validation/checkout.valid.js
+++ routes/Website Keraton/validation/checkout.valid.js
@@ -14,12 +14,12 @@ const validateCheckout = validate({
 })
 
 const validateCheckoutJanji = validate({
-    temp_cart: z.object(),
+    temp_cart: z.any(),
     temp_user_data: z.object({
         booker_name: z.string({ required_error: "Booker Name is required" }),
         booker_email: z.string({ required_error: "Booker Email is required" }),
         booker_phone: z.string({ required_error: "Booker Phone is required" }),
-        datetime: z.date({ required_error: "Datetime is required" }),
+        datetime: z.string({ required_error: "Datetime is required" }),
         availability_id: z.number({ required_error: "Availability ID is required" }).int(),
     }),
     method: z.string({
